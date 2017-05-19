c5ce539bf4b315c2ced624d144bfc916482f9815
Sam Welek <s.t.h.welek@edu.salford.ac.uk>
2017-05-19 01:15:14 +0100

    Initial Commit of Project
    Contains basic project files for a multiple tab project using Swift

---
+++ SEPA/SEPA/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SEPA
+//
+//  Created by Welek Samuel on 19/05/2017.
+//  Copyright © 2017 Welek Samuel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SEPA/SEPATests/SEPATests.swift
@@ -0,0 +1,36 @@
+//
+//  SEPATests.swift
+//  SEPATests
+//
+//  Created by Welek Samuel on 19/05/2017.
+//  Copyright © 2017 Welek Samuel. All rights reserved.
+//
+
+import XCTest
+@testable import SEPA
+
+class SEPATests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
