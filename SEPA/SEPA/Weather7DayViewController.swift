ed3c3ddbddc7c12b1054eaba2350a66fbf81ae5f
Sam Welek <s.t.h.welek@edu.salford.ac.uk>
2017-05-23 14:59:37 +0100

    Project Structure Changes and Weather Tab Bar View
    Changed the project structure, only inside XCode - the files are still where they are in the Finder. Now each element within the SEPA app is inside it's own group
    
    Also modified Weather section to be inside a Tab Bar Controller with a series of Sub-Views

--- SEPA/SEPA/Weather7DayViewController.swift
+++ SEPA/SEPA/Weather7DayViewController.swift
@@ -1,14 +1,14 @@
 //
-//  WeatherTodayViewController.swift
+//  Weather7DayViewController.swift
 //  SEPA
 //
-//  Created by Welek Samuel on 19/05/2017.
+//  Created by Welek Samuel on 23/05/2017.
 //  Copyright © 2017 Welek Samuel. All rights reserved.
 //
 
 import UIKit
 
-class WeatherTodayViewController: UIViewController {
+class Weather7DayViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,7 +18,5 @@ class WeatherTodayViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
---
+++ SEPA/SEPA/WeatherTabBarController.swift
@@ -0,0 +1,61 @@
+//
+//  WeatherTabBarController.swift
+//  SEPA
+//
+//  Created by Welek Samuel on 23/05/2017.
+//  Copyright © 2017 Welek Samuel. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class WeatherTabBarController: UITabBarController, CLLocationManagerDelegate {
+
+    let locationManager = CLLocationManager()
+    var coords = CLLocationCoordinate2D(latitude: 53.4846, longitude: -2.2708)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus){
+        
+        let newLat = String(format: "%f", coords.latitude)
+        let newLong = String(format: "%f", coords.longitude)
+        
+        print("Location Changed. Latitude: " + newLat + " Longitude: " + newLong)
+        
+        if (status == .AuthorizedAlways){
+            getLocation()
+        } else if (status == .Denied){
+            let alert = UIAlertController(title: "Error", message: "Goto Settings and allow this app to access your location", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func getLocation(){
+        if let loc = locationManager.location?.coordinate{
+            coords = loc
+        }
+        getWeatherData()
+        
+    }
+    
+    func getWeatherData(){
+        let url = WeatherURL(lat: String(coords.latitude), long: String(coords.longitude)).getFullURL()
+        print(url)
+    }
+
+}
