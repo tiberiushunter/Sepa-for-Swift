e8bcf5eb76c071663014a053b9484c3378dc904d
Sam Welek <s.t.h.welek@edu.salford.ac.uk>
2017-05-23 17:38:23 +0100

    Moved logic to the specific view controller
    Would be good to move all the logic into a seperate class

--- SEPA/SEPA/WeatherTabBarController.swift
+++ SEPA/SEPA/WeatherTabBarController.swift
@@ -10,79 +10,15 @@ import UIKit
 import MapKit
 
 class WeatherTabBarController: UITabBarController, CLLocationManagerDelegate {
-
-    let locationManager = CLLocationManager()
-    var coords = CLLocationCoordinate2D(latitude: 53.4846, longitude: -2.2708)
-    var temperature = 0.00
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestAlwaysAuthorization()
-        locationManager.startUpdatingLocation()
+
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus){
-        
-        let newLat = String(format: "%f", coords.latitude)
-        let newLong = String(format: "%f", coords.longitude)
-        
-        print("Location Changed. Latitude: " + newLat + " Longitude: " + newLong)
-        
-        if (status == .AuthorizedAlways){
-            getLocation()
-        } else if (status == .Denied){
-            let alert = UIAlertController(title: "Error", message: "Goto Settings and allow this app to access your location", preferredStyle: .Alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
-            self.presentViewController(alert, animated: true, completion: nil)
-        }
-    }
-    
-    func convertToCelsius(fahrenheit: Int) -> Int {
-        return Int(5.0 / 9.0 * (Double(fahrenheit) - 32.0))
-    }
-    
-    func getLocation(){
-        if let loc = locationManager.location?.coordinate{
-            coords = loc
-        }
-        getWeatherData()
-        
-    }
-    
-    func getWeatherData(){
-        let urla = WeatherURL(lat: String(coords.latitude), long: String(coords.longitude)).getFullURL()
-        print(urla)
-        
-        let urlPath = WeatherURL(lat: String(coords.latitude), long: String(coords.longitude)).getFullURL()
-        let url: NSURL = NSURL(string: urlPath)!
-        let request: NSURLRequest = NSURLRequest(URL: url)
-        let queue: NSOperationQueue = NSOperationQueue()
-        
-        NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler: { (response: NSURLResponse?, data: NSData?, error: NSError?) -> Void in
-            do {
-                if let jsonResult = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary {
-                    if let currently = jsonResult["currently"] as? Dictionary<String, AnyObject>{
-                        if let temperature = currently["temperature"] as? Double{
-                            self.temperature = temperature
-                        }
-                    }
-                }
-            
-                } catch let error as NSError {
-                    print(error.localizedDescription)
-                }
-        
-        })
-        
-    }
-
 }
