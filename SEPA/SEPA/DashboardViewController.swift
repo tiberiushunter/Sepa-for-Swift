3744a7e4caace59b85b2d1548d0d7bab59f53522
Sam Welek <s.t.h.welek@edu.salford.ac.uk>
2017-05-23 09:58:25 +0100

    Added Dashboard Table View
    As above

--- SEPA/SEPA/DashboardViewController.swift
+++ SEPA/SEPA/DashboardViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class DashboardViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
 
